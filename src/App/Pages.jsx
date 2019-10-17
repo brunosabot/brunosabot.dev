466ce079466d93eb4be7a422e7ecfd605033eae2
Bruno Sabot <brunosabot@gmail.com>
2019-10-17 23:13:16 +0200

    Handle prefers-reduced-motion

--- src/App/Pages.jsx
+++ src/App/Pages.jsx
@@ -6,9 +6,24 @@ import Articles from "../components/routes/Articles";
 import Projects from "../components/routes/Projects";
 import Talks from "../components/routes/Talks";
 
+let reduceMotion = false;
+
+(function() {
+  const updateReduceMotion = () => {
+    reduceMotion = mediaQuery.matches;
+  };
+
+  const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+  mediaQuery.addEventListener("change", updateReduceMotion);
+  updateReduceMotion();
+})();
+
 const Pages = () => {
   const { values } = useContext(AppContext);
   const transitions = useTransition(values.page, null, {
+    config: {
+      duration: reduceMotion ? 1 : undefined
+    },
     initial: { opacity: 1, transform: "scale3d(1, 1, 1) translate3d(0, 0, 0)" },
     from: { opacity: 0, transform: "scale3d(0.9, 0.9, 0.9) translate3d(0, 0, 0)" },
     enter: { opacity: 1, transform: "scale3d(1, 1, 1) translate3d(0, 0, 0)" },
