bd52fed02e010a8545ab4adcaf69e8c521b93e2f
Bruno Sabot <brunosabot@gmail.com>
2020-01-31 15:54:32 +0100

    Improve videos

--- src/components/routes/Talks.jsx
+++ src/components/routes/Talks.jsx
@@ -8,25 +8,36 @@ const Talks = () => {
 
   const talks = [
     {
+      id: 1,
+      conference: "Snowcamp",
+      language: "🇫🇷",
+      title: "L'intégration, ce purgatoire",
+      date: "2020-01-23",
+      slides: "https://drive.google.com/open?id=18gLNemp9mcsmx-RIh2RMHWFjiv4uvXu54ouW05BjGQE"
+    },
+    {
       id: "jnxkdHo8OEk",
       conference: "Breizh Camp",
       language: "🇫🇷",
       title: "UX : les formulaires",
-      date: "2019-03-21"
+      date: "2019-03-21",
+      slides: "https://drive.google.com/open?id=1tCwdzIdwu8jYX4Y0dej_u6pmc1oUx9Y4NPUaTBSWluA"
     },
     {
       id: "H2KWVDH64EY",
       conference: "Jug Summer Camp",
       language: "🇫🇷",
       title: "UX : les formulaires",
-      date: "2018-09-14"
+      date: "2018-09-14",
+      slides: "https://drive.google.com/open?id=1S2RmcMKZ74Bhc4IQUMm7-xDjVwD-_UCFc5iWCEdy5S8"
     },
     {
       id: "h0MAi-1GdZQ",
       conference: "BDX I/O",
       language: "🇫🇷",
       title: "Les nouveautés de React 16 - Fiber",
-      date: "2017-11-10"
+      date: "2017-11-10",
+      slides: "https://drive.google.com/open?id=1aMy3Szz-A-6hoTFuGvdVqoTZOEhc07W6j8emIcNb3Po"
     }
   ];
 
