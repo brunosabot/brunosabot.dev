0931ddeff42329602b55a232deb557780a514112
Bruno Sabot <brunosabot@gmail.com>
2020-07-05 18:38:17 +0200

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,6 +100,62 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return {
+                  ...edge.node.frontmatter,
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                };
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        path
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Bruno Sabot RSS Feed",
+            match: "^/posts/",
+            link: "https://feeds.feedburner.com/BlogBrunoSabot",
+          },
+        ],
+      },
+    },
     "gatsby-plugin-offline",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-client-side-redirect", // keep it in last in list
