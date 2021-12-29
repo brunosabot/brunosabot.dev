b902670b26fc1ddc780534d5e1385d803359f43f
Bruno Sabot <brunosabot@gmail.com>
2021-12-29 14:14:25 +0100

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,29 +1,34 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  poweredByHeader: false,
-  generateEtags: false,
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    exclude: [/googletagmanager/],
-    offlineGoogleAnalytics: false,
-    disable: process.env.NODE_ENV !== "production",
-  },
-  images: {
-    minimumCacheTTL: 864000,
-    domains: [
-      "storage.googleapis.com",
-      "lh3.googleusercontent.com",
-      "dodgemsandfloss.com",
-      "preview.redd.it",
-      "i3.ytimg.com",
-      "ifttd.io",
-      "img.youtube.com",
-      "images.unsplash.com",
-    ],
-  },
-});
+module.exports = withBundleAnalyzer(
+  withPWA({
+    poweredByHeader: false,
+    generateEtags: false,
+    reactStrictMode: true,
+    pwa: {
+      dest: "public",
+      runtimeCaching,
+      exclude: [/googletagmanager/],
+      offlineGoogleAnalytics: false,
+      disable: process.env.NODE_ENV !== "production",
+    },
+    images: {
+      minimumCacheTTL: 864000,
+      domains: [
+        "storage.googleapis.com",
+        "lh3.googleusercontent.com",
+        "dodgemsandfloss.com",
+        "preview.redd.it",
+        "i3.ytimg.com",
+        "ifttd.io",
+        "img.youtube.com",
+        "images.unsplash.com",
+      ],
+    },
+  })
+);
