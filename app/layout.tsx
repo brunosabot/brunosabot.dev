7a56e00ba725dc7d5cb5141c0db365f8e1a29c93
Bruno Sabot <brunosabot@gmail.com>
2022-12-24 18:49:28 +0100

    Fix viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,22 @@ interface IRootLayoutProps {
 export default function RootLayout({ children }: IRootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width" />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed for brunosabot.dev"
+          href="/rss.xml"
+        />
+        <link
+          rel="manifest"
+          href="/manifest.webmanifest"
+          crossOrigin="anonymous"
+        />
+        <link rel="apple-touch-icon" href="/icons/maskable_icon.png" />
+        <meta name="monetization" content="$ilp.uphold.com/zbywBq9qy3pe" />
+      </head>
       <body>
         {children}
         <div id="modal-root"></div>
