980bd105e4bbd989cfeaeba14adc65611bce447c
Bruno Sabot <brunosabot@gmail.com>
2023-02-21 21:12:23 +0100

    Improve UUID tool description

--- app/(default)/tools/(tools)/uuid/page.tsx
+++ app/(default)/tools/(tools)/uuid/page.tsx
@@ -124,13 +124,16 @@ export default function ToolUuidPage() {
       </Button>
 
       <SimpleCard>
-        The tool is a unique identifier generator that helps you quickly and
-        easily generate ids based on four different formats: UUID V1, UUID V4,
-        UUID V5, Nano ID and ULID. UUID V1 uses the current time and the
+        The updated tool is a unique identifier generator that helps you quickly
+        and easily generate ids based on five different formats: UUID V1, UUID
+        V4, UUID V5, Nano ID, and ULID. UUID V1 uses the current time and the
         device&apos;s MAC address to generate a unique id, while UUID V4 uses
         random numbers. UUID V5 uses a secure hash function to generate a unique
         id based on a given namespace and name, and Nano ID uses a secure random
-        number generator to generate a compact and URL-friendly id.
+        number generator to generate a compact and URL-friendly id. ULID is a
+        Universally Unique Lexicographically Sortable Identifier that uses the
+        current time and random numbers to generate a 128-bit id that is both
+        highly unique and easily sortable.
         <br />
         The tool provides a simple and intuitive interface for generating ids in
         each of these formats, allowing you to choose the best format for your
@@ -138,7 +141,7 @@ export default function ToolUuidPage() {
         results and make any necessary adjustments. Whether you&apos;re a
         developer, data analyst, or simply need to generate unique ids, this
         tool is a must-have tool for your toolkit. With its flexible and
-        easy-to-use interface, you can generate ids in any of the four formats
+        easy-to-use interface, you can generate ids in any of the five formats
         with just a few clicks.
       </SimpleCard>
     </>
