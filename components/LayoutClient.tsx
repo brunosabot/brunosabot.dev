123cd2427d3756e82b1797777c464bb58eb5c33a
Bruno Sabot <brunosabot@gmail.com>
2023-11-28 21:15:10 +0100

    chore: update and clean dependencies

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -1,26 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 import useKonami from "../lib/konami";
-// import { useRouter } from "next/router";
-import Analytics from "../components/Analytics";
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
 function LayoutClient() {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   const handleRouteChange = (url: string) => {
-  //     if (location.hostname === "localhost") return;
-
-  //     const ua = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ?? "";
-  //     window.gtag("config", ua, { page_path: url });
-  //   };
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
-
   useKonami();
 
   return (
