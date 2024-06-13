82150c693718ce6406228b88ab566782c10b1076
Bruno Sabot <brunosabot@gmail.com>
2024-06-13 14:41:11 +0200

    fix: caching method to stale while revalidate

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,29 +24,14 @@ if (workbox.navigationPreload.isSupported()) {
 
 workbox.routing.registerRoute(
   new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
+  new StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+self.addEventListener('fetch', event => {
+  if (event.request.url.endsWith('.png')) {
+    // Using the previously-initialized strategies will work as expected.
+    event.respondWith(new StaleWhileRevalidate({ cacheName: CACHE }).handle({event, request}));
   }
 });
