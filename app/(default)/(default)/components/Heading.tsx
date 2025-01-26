bf6b73b466e28e982839fee81f8a866add388646
Bruno Sabot <brunosabot@gmail.com>
2025-01-26 21:00:37 +0100

    fix: quote to apos

--- app/(default)/(default)/components/Heading.tsx
+++ app/(default)/(default)/components/Heading.tsx
@@ -3,7 +3,7 @@ import classNames from "./Heading.module.css";
 export default function Heading() {
   return (
     <h1 className={classNames.Heading}>
-      Hi, I'm <span className={classNames.Text}>Bruno</span>,<br />a Staff
+      Hi, I&apos;m <span className={classNames.Text}>Bruno</span>,<br />a Staff
       Software Engineer at{" "}
       <a
         className={classNames.Text}
